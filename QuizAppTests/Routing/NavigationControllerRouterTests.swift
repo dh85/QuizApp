d05ca362be14e145759dae71b495742bc76aeb54
David Hughes <39618866+dh85@users.noreply.github.com>
2021-04-12 15:59:16 +0100

    Improved a test name

--- QuizAppTests/Routing/NavigationControllerRouterTests.swift
+++ QuizAppTests/Routing/NavigationControllerRouterTests.swift
@@ -98,7 +98,7 @@ class NavigationControllerRouterTests: XCTestCase {
         XCTAssertTrue(callbackWasFired)
     }
 
-    func test_routeToResult_showsResultController() {
+    func test_didCompleteQuiz_showsResultController() {
         let viewController = UIViewController()
         let userAnswers = [(singleAnswerQuestion, ["A1"])]
 
